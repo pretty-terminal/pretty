1167cb2d65501a72e9fe546a84f9e54c80a14197
Sigmanificient <edhyjox@gmail.com>
2025-09-18 21:05:12 +0200

    refactor: make the configuration strictier

--- src/config.c
+++ src/config.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,6 +9,67 @@
 #include "macro_utils.h"
 
 static char CONFIG_PATH_SUFFIX[] = "/pretty/pretty.conf";
+
+enum type {
+    V_STRING,
+    V_NUMBER,
+    V_COLOR,
+};
+
+struct cval {
+    char const *section_name;
+    char const *key_name;
+    enum type value;
+    void *target;
+};
+
+static generic_config CONFIG = {
+    .font_name = "Jetbrains Mono Nerd Font",
+    .font_size = 12,
+    .win_padding = 12,
+    .color_palette = {
+        "000000FF",
+        "AA0000FF",
+        "00AA00FF",
+        "AA5500FF",
+        "0000AAFF",
+        "AA00AAFF",
+        "00AAAAFF",
+        "AAAAAAFF",
+        "555555FF",
+        "FF5555FF",
+        "55FF55FF",
+        "FFFF55FF",
+        "5555FFFF",
+        "FF55FFFF",
+        "55FFFFFF",
+        "FFFFFFFF",
+        [COLOR_BACKGROUND] = "000000FF"
+    }
+};
+
+static struct cval CONFIG_VALIDATION[] = {
+   { "font", "family", V_STRING, &CONFIG.font_name },
+   { "font", "size", V_NUMBER, &CONFIG.font_size },
+   { "window", "padding", V_NUMBER, &CONFIG.win_padding },
+   { "palette", "background", V_COLOR, CONFIG.color_palette[COLOR_BACKGROUND] },
+   { "palette", "color0", V_COLOR, CONFIG.color_palette[0] },
+   { "palette", "color1", V_COLOR, CONFIG.color_palette[1] },
+   { "palette", "color2", V_COLOR, CONFIG.color_palette[2] },
+   { "palette", "color3", V_COLOR, CONFIG.color_palette[3] },
+   { "palette", "color4", V_COLOR, CONFIG.color_palette[4] },
+   { "palette", "color5", V_COLOR, CONFIG.color_palette[5] },
+   { "palette", "color6", V_COLOR, CONFIG.color_palette[6] },
+   { "palette", "color7", V_COLOR, CONFIG.color_palette[7] },
+   { "palette", "color8", V_COLOR, CONFIG.color_palette[8] },
+   { "palette", "color9", V_COLOR, CONFIG.color_palette[9] },
+   { "palette", "color10", V_COLOR, CONFIG.color_palette[10] },
+   { "palette", "color11", V_COLOR, CONFIG.color_palette[11] },
+   { "palette", "color12", V_COLOR, CONFIG.color_palette[12] },
+   { "palette", "color13", V_COLOR, CONFIG.color_palette[13] },
+   { "palette", "color14", V_COLOR, CONFIG.color_palette[14] },
+   { "palette", "color15", V_COLOR, CONFIG.color_palette[15] },
+};
 
 char *get_default_config_file(void)
 {
@@ -49,75 +111,108 @@ char *get_default_config_file(void)
     return NULL;
 }
 
-static
-config_section *parse_config(char *cat_config, char **keys, int num_keys)
+char *parse_value(struct cval *p, char *s)
 {
-    int len_keys = num_keys;
+    unsigned long n;
 
-    config_section *settings = malloc(len_keys * sizeof(config_section));
-
-    if (settings == NULL) {
-        fprintf(stderr, "settings: Failed to allocate memory");
-        return NULL;
+    switch (p->value) {
+        case V_STRING:
+            *(char **)p->target = s;
+            s += strcspn(s, "\"\n");
+            if (*s != '\"')
+                fprintf(stderr, "Unterminated string!\n");
+            else s++;
+            break;
+        case V_NUMBER:
+            n = strtoul(s, &s, 10);
+            if (n > UCHAR_MAX) {
+                fprintf(stderr, "Value for [%s].[%s] is not in range 0-255!\n",
+                    p->section_name, p->key_name);
+                break;
+            }
+            *(unsigned char *)p->target = n;
+            break;
+        case V_COLOR:
+            if (*s != '\"')
+                fprintf(stderr, "Missing start quote!\n");
+            else s++;
+            if (*s == '#')
+                s++;
+            n = 0;
+            for (; isxdigit(s[n]); n++);
+            if (n != 6 && n != 8) {
+                fprintf(stderr,
+                    "Color [%s].[%s] must be in rgb(a) hex format: %s\n",
+                    p->section_name, p->key_name, s);
+                break;
+            }
+            memcpy(p->target, s, n);
+            s += n;
+            if (*s != '\"')
+                fprintf(stderr, "Missing end quote!\n");
+            else s++;
+            break;
     }
-
-    int i;
-
-    for (i = 0; i < len_keys; i++) {
-        char *key_pos = strstr(cat_config, keys[i]);
-        settings[i].name = keys[i];
-        settings[i].kvs.key = keys[i];
-
-        if (!key_pos) {
-            settings[i].kvs.value = NULL;
-            continue;
-        }
-
-        int key_id = (int)(key_pos - cat_config);
-        while (cat_config[key_id] && cat_config[key_id] != '=') key_id++;
-        if (cat_config[key_id] != '=') {
-            settings[i].kvs.value = NULL;
-            continue;
-        }
-        key_id+=2;
-        if (cat_config[key_id] == '"')
-            key_id++;
-
-        int start = key_id;
-        while (cat_config[key_id] && (cat_config[key_id] != '\n'
-            || cat_config[key_id] != '"'))
-            key_id++;
-        int len = key_id - start;
-
-        settings[i].kvs.value = malloc(len + 1);
-        if (settings[i].kvs.value) {
-            memcpy(settings[i].kvs.value, &cat_config[start], len);
-            settings[i].kvs.value[len] = '\0';
-        }
-    }
-
-    settings->count = i;
-
-    return settings;
+    return s;
 }
 
-
-config_struct *return_config(char *cat_config)
+static
+char *parse_key_value(char const *section_name, char *s)
 {
-    config_struct *config = malloc(sizeof(config_struct));
-    if (config == NULL) {
-        fprintf(stderr, "config: Failed to allocate memory");
-        return NULL;
+    char const *key = s;
+
+    for (; isalnum(*s); s++);
+    bool seen = *s == '=';
+    *s++ = '\0';
+    for (; isspace(*s); s++);
+    if (*s != '=' && !seen) {
+        fprintf(stderr, "Missing assignment operator\n");
+        return s;
+    }
+    s++;
+    for (; isspace(*s); s++);
+
+    struct cval *p = NULL;
+    for (size_t i = 0; i < length_of(CONFIG_VALIDATION); i++) {
+        if (!strcmp(CONFIG_VALIDATION[i].section_name, section_name)
+          && !strcmp(CONFIG_VALIDATION[i].key_name, key)) {
+            p = &CONFIG_VALIDATION[i];
+            break;
+        }
+    }
+    if (p == NULL) {
+        fprintf(stderr, "key [%s].[%s] is not a valid a setting\n",
+            section_name, key);
+        return s;
     }
 
-    char *font_keys[] = {"family", "size"};
-    config->font = parse_config(cat_config, font_keys, length_of(font_keys));
+    return parse_value(p, s);
+}
 
-    char *window[] = {"opacity", "padding"};
-    config->window = parse_config(cat_config, window, length_of(window));
+generic_config *return_config(char *cat_config)
+{
+    // TODO: write a even better parser
+    char const *section_name = "global";
 
-    char *pallete[] = {"background", "foreground"};
-    config->pallete = parse_config(cat_config, pallete, length_of(pallete));
-
-    return config;
+    for (char *s = cat_config; *s != '\0'; s++) {
+        if (*s == '[') {
+            section_name = ++s;
+            for (; isalpha(*s); s++);
+            if (*s != ']')
+                fprintf(stderr, "Unterminated config section!\n");
+            *s++ = '\0';
+            goto skip;
+        }
+        if (isalpha(*s)) {
+            s = parse_key_value(section_name, s);
+            goto skip;
+        }
+        if (*s == '#')
+            goto skip;
+        if (*s != '\n')
+            fprintf(stderr, "<<%.10s>> Garbage at the end of the line", s);
+skip:
+        s += strcspn(s, "\n");
+    }
+    return &CONFIG;
 }
--- src/config.h
+++ src/config.h
@@ -1,6 +1,9 @@
 #ifndef CONFIG_H
     #define CONFIG_H
 
+    #include "macro_utils.h"
+    #include <SDL3/SDL_pixels.h>
+
 typedef struct {
     char *key;
     char *value;
@@ -18,7 +21,20 @@ typedef struct {
     config_section *window;
 } config_struct;
 
-config_struct *return_config(char *cat_config);
+enum {
+  REVERSED_COLOR = 15,
+  COLOR_BACKGROUND,
+  COLOR_COUNT,
+};
+
+typedef struct {
+    char *font_name;
+    unsigned char font_size;
+    unsigned char win_padding;
+    char color_palette[COLOR_COUNT][length_of("rrggbbaa") - 1];
+} generic_config;
+
+generic_config *return_config(char *cat_config);
 char *get_default_config_file(void);
 
 #endif // CONFIG_H
