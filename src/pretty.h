ca868295570949fa4dc1961343a495130f662ce7
Gurjaka <esartia.gurika@gmail.com>
2025-09-27 18:29:05 +0400

    feat: init pty_new function

--- src/pretty.h
+++ src/pretty.h
@@ -1,6 +1,22 @@
 #ifndef LIB_H
     #define LIB_H
 
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <stdarg.h>
+
+static inline __attribute__((format(printf, 1, 2)))
+void die(const char *errstr, ...)
+{
+    va_list ap;
+    va_start(ap, errstr);
+
+    vfprintf(stderr, errstr, ap);
+    va_end(ap);
+
+    exit(EXIT_FAILURE);
+}
+
 char *file_read(char const *filepath);
 
 #endif
---
+++ src/slave.h
@@ -0,0 +1,16 @@
+#ifndef SLAVE_H
+    #define SLAVE_H
+
+enum term_mode {
+	MODE_WRAP        = 1 << 0,
+	MODE_INSERT      = 1 << 1,
+	MODE_ALTSCREEN   = 1 << 2,
+	MODE_CRLF        = 1 << 3,
+	MODE_ECHO        = 1 << 4,
+	MODE_PRINT       = 1 << 5,
+	MODE_UTF8        = 1 << 6,
+};
+
+int tty_new(const char *line, char *cmd, const char *out, char **args);
+
+#endif // SLAVE_H
