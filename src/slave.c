0997134b1d998cc673331342d339e502fb850244
Sigmanificient <edhyjox@gmail.com>
2025-10-04 00:50:30 +0200

    refactor: focus on a minimal implementation

--- src/slave.c
+++ src/slave.c
@@ -10,27 +10,20 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-#include "macro_utils.h"
 #include "pretty.h"
 #include "slave.h"
 #include "sys/ioctl.h"
 
-static int iofd = 1;
-int cmdfd;
 static pid_t pid;
-
-char *scroll;
-char *utmp;
 
 void
 sigchld(int a)
 {
 	int stat;
-	pid_t p;
+	pid_t p = waitpid(pid, &stat, WNOHANG);
 
-	if ((p = waitpid(pid, &stat, WNOHANG)) < 0)
+	if (p < 0)
 		die("waiting for pid %hd failed: %s\n", pid, strerror(errno));
-
 	if (pid != p)
 		return;
 
@@ -41,9 +34,8 @@ sigchld(int a)
 }
 
 static
-void exec_sh(char *cmd, char **args)
+void exec_sh(char *args[static 1])
 {
-    char *shell, *prog, *arg;
     const struct passwd *pw;
 
     errno = 0;
@@ -51,34 +43,15 @@ void exec_sh(char *cmd, char **args)
         die("getpwuid: %s\n", errno != 0 ? strerror(errno) : "unknown error");
     }
 
-    if ((shell = getenv("SHELL")) == NULL)
-        shell = (pw->pw_shell[0]) ? pw->pw_shell : cmd;
-
-    if (args) {
-        prog = args[0];
-        arg = NULL;
-    } else if (scroll) {
-        prog = scroll;
-        arg = utmp ? utmp : shell;
-    } else if (utmp) {
-        prog = utmp;
-        arg = NULL;
-    } else {
-        prog = shell;
-        arg = NULL;
-    }
-
-    default_value(args, ((char *[]) {prog, arg, NULL}));
-
 	unsetenv("COLUMNS");
 	unsetenv("LINES");
 	unsetenv("TERMCAP");
 	setenv("LOGNAME", pw->pw_name, 1);
 	setenv("USER", pw->pw_name, 1);
-	setenv("SHELL", shell, 1);
+    // TODO: we will handle real shell shenanigans later
+	// setenv("SHELL", shell, 1);
 	setenv("HOME", pw->pw_dir, 1);
 	setenv("TERM", "pretty", 1);
-
 
 	signal(SIGCHLD, SIG_DFL);
 	signal(SIGHUP, SIG_DFL);
@@ -89,32 +62,14 @@ void exec_sh(char *cmd, char **args)
 
     execvp(args[0], args);
     perror("execvp failed");
-    _exit(1);
+    exit(EXIT_FAILURE);
 }
 
-int tty_new(const char *line, char *cmd, const char *output, char **args)
+int tty_new(char *args[static 1])
 {
-    int master, slave;
-
-    if (output != NULL) {
-        iofd = (!strcmp(output, "-")) ?
-            1 : open(output, O_WRONLY | O_CREAT, 0666);
-
-        if (iofd < 0) {
-            fprintf(stderr, "Error opening %s%s\n",
-                output, strerror(errno));
-        }
-    }
-
-    // TODO: implement stty function
-	// if (line) {
-	// 	if ((cmdfd = open(line, O_RDWR)) < 0)
-	// 		die("open line '%s' failed: %s\n",
-	// 		    line, strerror(errno));
-	// 	dup2(cmdfd, 0);
-	// 	stty(args);
-	// 	return cmdfd;
-	// }
+    int cmdfd = -1;
+    int master;
+    int slave;
 
     if (openpty(&master, &slave, NULL, NULL, NULL) < 0)
         die("openpty call failed: %s\n", strerror(errno));
@@ -136,8 +91,7 @@ int tty_new(const char *line, char *cmd, const char *output, char **args)
         if (slave > 2)
             close(slave);
 
-        exec_sh(cmd, args);
-        fprintf(stderr, "Successfully executed SH\n");
+        exec_sh(args);
         break;
     default:
         close(slave);
