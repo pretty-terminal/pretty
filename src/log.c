baf360059db3c6aa24d6481e2964fe9b66cfb64b
Gurjaka <esartia.gurika@gmail.com>
2025-10-15 22:36:07 +0400

    fix: add thread safety to pretty_log using pthread mutex

--- src/log.c
+++ src/log.c
@@ -1,20 +1,20 @@
-#include <string.h>
 #include <stdio.h>
 #include <assert.h>
 #include <time.h>
 #include <unistd.h>
 #include <stdarg.h>
+#include <pthread.h>
 
 #include "log.h"
-#include "macro_utils.h"
 
 void pretty_log(enum log_level level, const char *text, ...)
 {
+    static pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER;
+    
+    pthread_mutex_lock(&log_mutex);
 
     char message[BUFSIZ - 64];
-    char buffer[BUFSIZ];
     char timebuf[16];
-    ssize_t result;
     time_t now = time(NULL);
     struct tm local;
     localtime_r(&now, &local);
@@ -22,9 +22,8 @@ void pretty_log(enum log_level level, const char *text, ...)
     strftime(timebuf, sizeof(timebuf), "%H:%M:%S", &local);
 
     if (text == NULL) {
-        snprintf(buffer, sizeof(buffer), "[%s] PRETTY_ERROR: Null message passed to logger\n", timebuf);
-        result = write(STDERR_FILENO, buffer, strlen(buffer));
-        UNUSED(result);
+        fprintf(stderr, "[%s] PRETTY_ERROR: Null message passed to logger\n", timebuf);
+        pthread_mutex_unlock(&log_mutex);
         return;
     }
 
@@ -33,32 +32,26 @@ void pretty_log(enum log_level level, const char *text, ...)
     vsnprintf(message, sizeof(message), text, args);
     va_end(args);
 
-    int fd;
     const char *logstr;
 
     switch (level) {
     case PRETTY_ERROR:
-        fd = STDERR_FILENO;
         logstr = "PRETTY_ERROR";
         break;
     case PRETTY_WARN:
-        fd = STDERR_FILENO;
         logstr = "PRETTY_WARN";
         break;
     case PRETTY_INFO:
-        fd = STDOUT_FILENO;
         logstr = "PRETTY_INFO";
         break;
     case PRETTY_DEBUG:
-        fd = STDOUT_FILENO;
         logstr = "PRETTY_DEBUG";
         break;
     default:
+        pthread_mutex_unlock(&log_mutex);
         return;
     }
 
-    snprintf(buffer, sizeof(buffer), "[%s | %s]: %s\n", timebuf, logstr, message);
-
-    result = write(fd, buffer, strlen(buffer));
-    UNUSED(result);
+    fprintf(stderr, "[%s | %s]: %s\n", timebuf, logstr, message);
+    pthread_mutex_unlock(&log_mutex);
 }
