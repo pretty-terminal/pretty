df964bd45f9f26613446b5cd943f703720dd01fc
Gurjaka <esartia.gurika@gmail.com>
2025-10-17 21:02:16 +0400

    refactor(logger): separate time retrieval, simplify logging macro

--- src/log.h
+++ src/log.h
@@ -8,7 +8,20 @@ enum log_level {
     PRETTY_DEBUG
 };
 
-void pretty_log(enum log_level level, const char *text, ...)
+void pretty_log_full(enum log_level level, const char *text, ...)
     __attribute__((format(printf, 2, 3)));
+
+char *pretty_log_get_time(void);
+
+#ifdef DEBUG_MODE
+    #define LOG_HEAD "[%s | %s] @ %s:%d: "
+    #define LOG_HEAD_ARGS(lvl) pretty_log_get_time(), #lvl, __FILE__, __LINE__
+#else
+    #define LOG_HEAD "[%s | %s]: "
+    #define LOG_HEAD_ARGS(lvl) pretty_log_get_time(), #lvl
+#endif
+
+#define pretty_log(lvl, msg, ...) \
+    pretty_log_full(lvl, LOG_HEAD msg, LOG_HEAD_ARGS(lvl), ##__VA_ARGS__)
 
 #endif // LOG_H
