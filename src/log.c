b93ae1e4e59ea3dec3e86f17ce109d2dcfcf2b09
Gurjaka <esartia.gurika@gmail.com>
2025-10-17 21:02:16 +0400

    refactor(logger): separate time retrieval, simplify logging macro

--- src/log.c
+++ src/log.c
@@ -7,51 +7,30 @@
 
 #include "log.h"
 
-void pretty_log(enum log_level level, const char *text, ...)
+char *pretty_log_get_time(void)
 {
-    static pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER;
-    
-    pthread_mutex_lock(&log_mutex);
-
-    char message[BUFSIZ - 64];
-    char timebuf[16];
+    static char timebuf[16];
     time_t now = time(NULL);
     struct tm local;
     localtime_r(&now, &local);
 
     strftime(timebuf, sizeof(timebuf), "%H:%M:%S", &local);
 
-    if (text == NULL) {
-        fprintf(stderr, "[%s] PRETTY_ERROR: Null message passed to logger\n", timebuf);
-        pthread_mutex_unlock(&log_mutex);
-        return;
-    }
+    return timebuf;
+}
+
+void pretty_log_full(enum log_level level, const char *text, ...)
+{
+    static pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER;
+    pthread_mutex_lock(&log_mutex);
+
+    char message[BUFSIZ - 64];
 
     va_list args;
     va_start(args, text);
     vsnprintf(message, sizeof(message), text, args);
     va_end(args);
 
-    const char *logstr;
-
-    switch (level) {
-    case PRETTY_ERROR:
-        logstr = "PRETTY_ERROR";
-        break;
-    case PRETTY_WARN:
-        logstr = "PRETTY_WARN";
-        break;
-    case PRETTY_INFO:
-        logstr = "PRETTY_INFO";
-        break;
-    case PRETTY_DEBUG:
-        logstr = "PRETTY_DEBUG";
-        break;
-    default:
-        pthread_mutex_unlock(&log_mutex);
-        return;
-    }
-
-    fprintf(stderr, "[%s | %s]: %s\n", timebuf, logstr, message);
+    fprintf(stderr, "%s\n", message);
     pthread_mutex_unlock(&log_mutex);
 }
