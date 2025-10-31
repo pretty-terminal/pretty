d7a5a6c5c7cc14744946afc453ce677a9d5e5606
Gurjaka <esartia.gurika@gmail.com>
2025-11-01 01:28:44 +0400

    feat: replace linear PTY buffer with ring buffer implementation

--- src/slave.h
+++ src/slave.h
@@ -16,21 +16,29 @@ enum term_mode {
 	MODE_UTF8        = 1 << 6,
 };
 
+enum { TTY_RING_CAP = BUFSIZ };
+
 typedef struct {
     int pty_master_fd;
-    char buff[BUFSIZ]; /* TODO: think about this */
-    size_t buff_len;
-    size_t buff_consumed; // how much has UI proccessed
+
+    char buff[TTY_RING_CAP]; /* TODO: think about this */
+    size_t head;
+    size_t tail;
+
     pthread_t thread;
     pthread_mutex_t lock;
+
     bool buff_changed;
     bool should_exit;
-    bool overflowed;
+
+    bool overwrite_oldest;
 } tty_state;
 
 int tty_new(char *args[static 1]);
 void *tty_poll_loop(void *arg);
 void tty_write(tty_state *tty, const char *s, size_t n);
 void tty_erase_last(tty_state *tty);
+size_t ring_read_span(const tty_state *tty, const char **ptr);
+void ring_consume(tty_state *tty, size_t k);
 
 #endif // SLAVE_H
